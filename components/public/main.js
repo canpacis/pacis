9130a5d38418f0318c319e4745a001f7a7bf4102
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-03-31 18:33:54 +0300

    implement http streaming on client side

--- components/public/main.js
+++ components/public/main.js
@@ -19,3 +19,20 @@ document.addEventListener("alpine:init", () => {
     return (subject) => navigator.clipboard.writeText(subject);
   });
 });
+
+window.addEventListener("load", () => {
+  const slotItems = Array.from(document.querySelectorAll("[slot]"))
+  for (const item of slotItems) {
+    const id = item.getAttribute("slot")
+    const wrapper = document.querySelector(`#${id}`)
+    if (!wrapper) {
+      return
+    }
+
+    const slot = wrapper.shadowRoot.querySelector(`slot[name='${id}']`)
+    if (!slot) {
+      return
+    }
+    slot.replaceWith(item)
+  }
+})
