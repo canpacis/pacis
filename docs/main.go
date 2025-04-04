b0c30fa6fb829a5d91d096d1079f247672b38519
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-04-04 23:34:43 +0300

    feat@docs: implement the new router

--- docs/main.go
+++ docs/main.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"embed"
+	"fmt"
 	"net/http"
 
 	"github.com/canpacis/pacis/docs/app"
-	"github.com/canpacis/pacis/pages"
-	"github.com/canpacis/pacis/ui/components"
-	"github.com/canpacis/pacis/ui/html"
+	. "github.com/canpacis/pacis/pages"
 )
 
+//go:embed public
+var public embed.FS
+
+func Theme(ctx Context) Context {
+	fmt.Println(ctx)
+	return ctx
+	// return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Println(r.URL)
+	// 	cookie, err := r.Cookie("pacis_color_scheme")
+	// 	if err != nil {
+	// 		switch cookie.Value {
+	// 		case "light":
+	// 		case "dark":
+	// 		default:
+	// 			// delete cookie
+	// 		}
+	// 	}
+	// 	h.ServeHTTP(w, r)
+	// })
+}
+
 func main() {
-	mux := http.NewServeMux()
-	head := components.CreateHead("/ui/")
+	router := Routes(
+		Public(public, "public"),
+		Layout(app.Layout),
+		Page(app.HomePage),
 
-	mux.Handle("/", pages.NewPageRoute("/", app.HomeLayout, func(pc *pages.PageContext) html.I {
-		return html.Div(html.Text("Hello, World!"))
-	}))
-	mux.Handle("/ui/", http.StripPrefix("/ui/", head.Handler()))
-	mux.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./docs/public"))))
+		Route(
+			Path("docs"),
+			Layout(app.DocLayout),
 
-	http.ListenAndServe(":8080", mux)
+			Route(Path("introduction"), Page(app.Introduction), Middleware(Theme)),
+			Route(Path("installation"), Page(app.Installation)),
+			Route(Path("components"), Redirect("/docs/alert")),
+			Route(Path("alert"), Page(app.AlertPage)),
+		),
+	)
+
+	http.ListenAndServe(":8080", router.Handler())
 }
