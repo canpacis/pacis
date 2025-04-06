64b8cc4ebc166aaa4513be5e6110b1dc82bb9d6f
Muhammed Ali CAN <tmwwdw@gmail.com>
2025-04-06 11:04:36 +0300

    feat@pages: add i18n middleware

--- pages/i18n/i18n.go
+++ pages/i18n/i18n.go
@@ -3,20 +3,19 @@ package i18n
 import (
 	"bytes"
 	"context"
+	"fmt"
 	"io"
 	"io/fs"
-	"net/http"
 	p "path"
 
+	"github.com/canpacis/pacis/pages"
 	c "github.com/canpacis/pacis/ui/components"
 	h "github.com/canpacis/pacis/ui/html"
 	"github.com/nicksnyder/go-i18n/v2/i18n"
 	"golang.org/x/text/language"
 )
 
-var bundle *i18n.Bundle
-
-func Setup(dir fs.FS, defaultlang language.Tag) error {
+func Setup(dir fs.FS, defaultlang language.Tag) (*i18n.Bundle, error) {
 	paths := []string{}
 	err := fs.WalkDir(dir, ".", func(path string, d fs.DirEntry, err error) error {
 		if err != nil {
@@ -31,18 +30,18 @@ func Setup(dir fs.FS, defaultlang language.Tag) error {
 		return nil
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	bundle = i18n.NewBundle(defaultlang)
+	bundle := i18n.NewBundle(defaultlang)
 	for _, path := range paths {
 		_, err = bundle.LoadMessageFileFS(dir, path)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	return nil
+	return bundle, nil
 }
 
 type Message struct {
@@ -51,20 +50,9 @@ type Message struct {
 }
 
 func (m Message) Render(ctx context.Context, w io.Writer) error {
-	var localizer *i18n.Localizer
-	rctx, ok := ctx.(interface{ Request() *http.Request })
-	if ok {
-		langs := []string{}
-		req := rctx.Request()
-		locale, err := req.Cookie("app-locale")
-
-		if err == nil {
-			langs = append(langs, locale.Value)
-		}
-		langs = append(langs, req.FormValue("lang"), req.Header.Get("Accept-Language"))
-		localizer = i18n.NewLocalizer(bundle, langs...)
-	} else {
-		localizer = i18n.NewLocalizer(bundle, language.English.String())
+	localizer := pages.Get[*i18n.Localizer](ctx, "localizer")
+	if localizer == nil {
+		return c.ErrorText(fmt.Errorf("no localizer in the context, have you registered the i18n middleware correctly?")).Render(ctx, w)
 	}
 
 	message, err := localizer.Localize(&i18n.LocalizeConfig{
@@ -90,7 +78,7 @@ func (m Message) String(ctx context.Context) string {
 	return buf.String()
 }
 
-func T(key string, data ...any) Message {
+func Text(key string, data ...any) Message {
 	var d any
 	if len(data) > 0 {
 		d = data[0]
