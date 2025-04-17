91bb7672627828562fd5690690db2284fff5da6c
canpacis <tmwwdw@gmail.com>
2025-04-17 20:08:48 +0300

    fix@www: remove console.log call

--- www/app/assets/main.ts
+++ www/app/assets/main.ts
@@ -1,6 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("a").forEach((a) => {
-    console.log(a);
     if (a.host !== window.location.host && a.href.startsWith("http")) {
       a.setAttribute("data-umami-event", "outbound-link-click");
       a.setAttribute("data-umami-event-url", a.href);
